f07d8a798b9e948b676e517a777f068b3f1e5774
Cortina <rafa@cortina.dev>
2026-08-28 19:32:18 +0200

    chore: bump version to 1.0.1 (code 3)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.cortinadev.dogmatix"
         minSdk = 29
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
