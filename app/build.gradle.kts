ce92262cd9dd431f978f07a2617528e86c39dc92
Cortina <rafa@cortina.dev>
2026-09-02 01:52:09 +0200

    chore: release 1.2 (versionCode 11)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.cortinadev.dogmatix"
         minSdk = 29
         targetSdk = 36
-        versionCode = 10
-        versionName = "1.1.4"
+        versionCode = 11
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
